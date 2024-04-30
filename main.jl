bd265eab5f251cb076dbe0f4e83e7a942d900240
Andrin Rehmann <andrinrehmann@gmail.com>
2024-04-30 14:56:48 +0200

    progress with enzyme usage

--- main.jl
+++ main.jl
@@ -1,91 +1,150 @@
-using Plots, CUDA, Distances
+using Plots, CUDA, Random, Enzyme
 
 dt = Float32(0.01)
 N = 1000
 M = 10
-steps = 50
+k = 10
 epsilon = 1e-6
 
-struct Particle{T}
-    pos_x::T
-    pos_y::T
-    vel_x::T
-    vel_y::T
-end
+function step!(
+    pos_before :: CuArray{Float32, 2, CUDA.Mem.DeviceBuffer},
+    pos_after :: CuArray{Float32, 2, CUDA.Mem.DeviceBuffer},
+    vel_before :: CuArray{Float32, 2, CUDA.Mem.DeviceBuffer},
+    vel_after :: CuArray{Float32, 2, CUDA.Mem.DeviceBuffer},
+    attr_pos :: CuArray{Float32, 2, CUDA.Mem.DeviceBuffer},
+    attr_s :: CuArray{Float32, 1, CUDA.Mem.DeviceBuffer},
+    dt :: Float32)
 
-struct Attractor{T}
-    pos_x::T
-    pos_y::T
-    strength::T
-end
-
-particles = Particle{CuArray{Float32, 1}}(
-    CUDA.rand(Float32, N) .- 0.5,
-    CUDA.rand(Float32, N) .- 0.5,
-    CUDA.rand(Float32, N) .- 0.5,
-    CUDA.rand(Float32, N) .- 0.5
-)
-
-pos_x_h_0 = Array(particles.pos_x)
-pos_y_h_0 = Array(particles.pos_y)
-
-attractors = Attractor{CuArray{Float32, 1}}(
-    CUDA.rand(Float32, M) .- 0.5,
-    CUDA.rand(Float32, M) .- 0.5,
-    CUDA.rand(Float32, M)
-)
-
-function step!(particles :: Particle, attractors :: Attractor, dt :: Float32)
-    diff_x = particles.pos_x .- attractors.pos_x'
-    diff_y = particles.pos_y .- attractors.pos_y'
+    diff_x = pos_before[1, :] .- attr_pos[1, :]'
+    diff_y = pos_before[2, :] .- attr_pos[2, :]'
 
     distance_sq = (diff_x.^2 .+ diff_y.^2) .+ epsilon
     
-    forces = attractors.strength' ./ distance_sq
+    forces = attr_s' ./ distance_sq
 
     forces_x = forces .* diff_x
-    forces_y = forces .* diff_y
+    forces_y = forces .* diff_y 
 
-    particles.vel_x .-= sum(forces_x, dims=2)
-    particles.vel_y .-= sum(forces_y, dims=2)
+    vel_after[1, :] = vel_before .- sum(forces_x, dims=2) * dt
+    vel_after[2, :] = vel_before .- sum(forces_y, dims=2) * dt
 
-    particles.pos_x .+= particles.vel_x .* dt
-    particles.pos_y .+= particles.vel_y .* dt
+    pos_after[1, :] .+= vel_after[1, :] .* dt
+    pos_after[2, :] .+= vel_after[2, :] .* dt
 
+    return nothing
 end
 
-println("Running simulation...")
+pos_0_h = zeros(Float32, (k, 2, N))
+pos_0_h[1, :, :] .= Random.rand(Float32, (2, N)) .- 0.5
 
-for i in 1:steps
-    step!(particles, attractors, dt)
+vel_0_h = zeros(Float32, (k, 2, N))
+attr_pos_h = Random.rand(Float32, (2, M)) .- 0.5
+attr_s_h = Random.rand(Float32, M)
+
+pos_d = CuArray{Float32}(pos_0_h)
+vel_d = CuArray{Float32}(vel_0_h)
+attr_pos_d = CuArray{Float32}(attr_pos_h)
+attr_s_d = CuArray{Float32}(attr_s_h)
+
+function forward!(
+    pos_hist :: CuArray{Float32, 3, CUDA.Mem.DeviceBuffer},
+    vel_hist :: CuArray{Float32, 3, CUDA.Mem.DeviceBuffer},
+    attr_pos :: CuArray{Float32, 2, CUDA.Mem.DeviceBuffer},
+    attr_s :: CuArray{Float32, 1, CUDA.Mem.DeviceBuffer},
+    dt :: Float32,
+    steps :: Int)
+
+    for i in 1:(steps-1)
+        step!(
+            pos_hist[i, :, :], pos_hist[i+1, :, :], 
+            vel_hist[i, :, :], vel_hist[i+1, :, :],
+            attr_pos, attr_s, dt)
+    end
+
+    return nothing
 end
 
-println("Done!")
+function loss(
+    pos :: CuArray{Float32, 2, CUDA.Mem.DeviceBuffer},
+    pos_star :: CuArray{Float32, 2, Cuda.Mem.DeviceBuffer},
+    loss :: CuArray{Float32, 2, Cuda.Mem.DeviceBuffer})
+
+    return sum(pos - pos_star .^2)
+
+function backward!(
+    pos_hist :: CuArray{Float32, 3, CUDA.Mem.DeviceBuffer},
+    pos_star :: CuArray{Float32, 2, CUDA.Mem.DeviceBuffer},
+    adj_pos_before :: CuArray{Float32, 2, CUDA.Mem.DeviceBuffer},
+    adj_pos_after :: CuArray{Float32, 2, CUDA.Mem.DeviceBuffer},
+    loss : CuArray{Float32, 1, CUDA.Mem.DeviceBuffer},
+    vel_hist :: CuArray{Float32, 3, CUDA.Mem.DeviceBuffer},
+    attr_pos :: CuArray{Float32, 2, CUDA.Mem.DeviceBuffer},
+    attr_s :: CuArray{Float32, 1, CUDA.Mem.DeviceBuffer},
+    dt :: Float32,
+    steps :: Int)
+
+    loss_value = loss(pos_hist[end, :, :], pos_star, loss)
+
+    autodiff(
+        Reverse,
+        loss,
+        Duplicated(pos_hist[end, :, :], b_pos),
+
+    for i in steps:1
+        pos_before = pos_hist[i, :, :]
+        pos_after = pos_hist[i+1, :, :]
+
+        autodiff(
+            Reverse,
+            step,
+            Duplicated(pos_before, adj_pos_before),
+            Duplicated(pos_after, adj_pos_after),
+            Const(vel_hist[i, :, :]),
+            Const(vel_hist[i+1, :, :]),
+            Const(attr_pos),
+            Const(attr_s),
+            Const(dt))
+    end
+end
+
+# forward pass
+forward!(
+    pos_d,
+    vel_d,
+    attr_pos_d,
+    attr_s_d,
+    dt,
+    steps)
+ 
+# backward pass
+backward!(
+    pos_d,
+    vel_d,
+    attr_pos_d,
+    attr_s_d,
+    dt,
+    steps)
 
 # transfer particles positions to host
-pos_x_h_T = Array(particles.pos_x)
-pos_y_h_T = Array(particles.pos_y)
-
-attr_pos_x_h = Array(attractors.pos_x)
-attr_pos_y_h = Array(attractors.pos_y)
+pos_h = Array(pos_d)
 
 p1 = scatter(
-    pos_x_h_0, pos_y_h_0,
+    pos_0_h[1, :], pos_0_h[2, :],
     label="Particle", markersize=2, 
     markerstrokewidth=0, xlims=(-1, 1), ylims=(-1, 1))
 
 p1 = scatter!(
-    attr_pos_x_h, attr_pos_y_h,
-    label="Attractor", markersize=10, markerstrokewidth=0)
+    attr_pos_h[1, :], attr_pos_h[2, :],
+    label="Attractor", markersize=5, markerstrokewidth=0)
 
 p2 = scatter(
-    pos_x_h_T, pos_y_h_T,
+    pos_h[1, :], pos_h[2, :],
     label="Particle", markersize=2, 
-    markerstrokewidth=0, xlims=(-5, 5), ylims=(-5, 5))
+    markerstrokewidth=0, xlims=(-1, 1), ylims=(-1, 1))
 
 p2 = scatter!(
-    attr_pos_x_h, attr_pos_y_h,
-    label="Attractor", markersize=4, markerstrokewidth=0)
+    attr_pos_h[1, :], attr_pos_h[2, :],
+    label="Attractor", markersize=5, markerstrokewidth=0)
 
 plt = plot(p1, p2, layout=(1, 2), size=(1200, 600))
 
